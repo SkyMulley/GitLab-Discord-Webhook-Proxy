7237e25ce805665db8427001d444921fffe6d85e
Sky Mulley <skymulley@gmail.com>
2020-05-08 20:14:04 +0100

    More stuff

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Static pages, like configuration ui
 app.use(express.static(path.resolve(__dirname, 'public')));
